f9013ffa96879361c50f69bcdb2c2a9084f6a05b
jianxun <v-xdeng@microsoft.com>
2022-08-09 09:34:44 +0000

    add some script

---
+++ script_user-item/data/process_taobao_amazon_data.sh
@@ -0,0 +1,38 @@
+PROJECT_ROOT='/media/xreco/jianxun/xGCN'
+ALL_DATA_ROOT='/media/xreco/DEV/xiran/data/social_and_user_item'
+
+ALL_DATASETS_ROOT=$ALL_DATA_ROOT'/datasets'
+
+######################################
+DATASET='taobao-1.6m'
+
+DATA_ROOT=$ALL_DATASETS_ROOT'/instance_'$DATASET
+FILE_INPUT='/media/xreco/DEV/xiran/data/user_item_lightgcn/datasets/instance_taobao-1.6m/train_edges.txt'
+
+python $PROJECT_ROOT'/'data/handle_edge_list_txt.py $PROJECT_ROOT \
+    --data_root $DATA_ROOT \
+    --dataset_type 'user-item' \
+    --dataset_name $DATASET \
+    --file_input $FILE_INPUT \
+
+
+FILE_INPUT='/media/xreco/DEV/xiran/data/user_item_lightgcn/datasets/instance_taobao-1.6m/val_pos_edges.txt'
+FILE_OUTPUT=$DATA_ROOT'/val_edges.pkl'
+
+python $PROJECT_ROOT'/'data/handle_src_pos_neg_eval_set.py $PROJECT_ROOT \
+    --file_input $FILE_INPUT \
+    --file_output $FILE_OUTPUT \
+
+
+python $PROJECT_ROOT'/'data/sample_numpy_array.py $PROJECT_ROOT \
+    --file_input $DATA_ROOT'/val_edges.pkl' \
+    --file_output $DATA_ROOT'/val_edges-1000.pkl' \
+    --num_sample 1000 \
+
+
+FILE_INPUT='/media/xreco/DEV/xiran/data/user_item_lightgcn/datasets/instance_taobao-1.6m/test_pos_edges.txt'
+FILE_OUTPUT=$DATA_ROOT'/test_edges.pkl'
+
+python $PROJECT_ROOT'/'data/handle_src_pos_neg_eval_set.py $PROJECT_ROOT \
+    --file_input $FILE_INPUT \
+    --file_output $FILE_OUTPUT \
--- script_user-item/xgcn/run_xgcn_yelp2018.sh
+++ script_user-item/xgcn/run_xgcn_yelp2018.sh
@@ -7,17 +7,25 @@ CONFIG_ROOT=$PROJECT_ROOT'/config'
 ALL_DATASETS_ROOT=$ALL_DATA_ROOT'/datasets'
 ALL_RESULTS_ROOT=$ALL_DATA_ROOT'/model_outputs'
 
-DATASET='gowalla'
+DATASET='yelp2018'
 
 DATA_ROOT=$ALL_DATASETS_ROOT'/instance_'$DATASET
 
 ################
 SEED=1
-RESULTS_DIR='xgcn/[0]'
+RESULTS_DIR='xgcn/[2dnn][2layer-ffn][prop1][reg-3][T5-Kinf][not-load_best]'
+# RESULTS_DIR='xgcn/[0]'
 
 RESULTS_ROOT=$ALL_RESULTS_ROOT'/gnn_'$DATASET'/'$RESULTS_DIR
-T=2
-K=10
+T=5
+K=999999
+
+LOAD_BEST=0
+
+USE_SNN=1
+USE_TWO_REFNET=1
+
+L2_REG=1e-3
 
 mkdir -p $RESULTS_ROOT
 LOG_FILE=$RESULTS_ROOT'/log.txt'
@@ -40,12 +48,19 @@ python $PROJECT_ROOT'/'main/main.py $PROJECT_ROOT \
     --mask_nei_when_test 1 \
     --file_test $DATA_ROOT'/test.pkl' \
     --key_score_metric 'r20' \
-    --convergence_threshold 50 \
+    --convergence_threshold 30 \
     --train_dl 'EdgeBased_Full_TrainDataLoader' \
     --prop_type 'lightgcn' --num_gcn_layers 1 --stack 1 \
-    --use_special_dnn 1 --dnn_arch '[torch.nn.Linear(64, 1024), torch.nn.Tanh(), torch.nn.Linear(1024, 1024), torch.nn.Tanh(), torch.nn.Linear(1024, 64)]' \
+    --use_special_dnn $USE_SNN --dnn_arch '[torch.nn.Linear(64, 1024), torch.nn.Tanh(), torch.nn.Linear(1024, 64)]' \
+    --scale_net_arch '[torch.nn.Linear(64, 32), torch.nn.Tanh(), torch.nn.Linear(32, 1), torch.nn.Sigmoid()]' \
+    --use_two_dnn $USE_TWO_REFNET \
     --renew_and_prop_freq $T --K $K --endure 3 \
+    --renew_by_loading_best $LOAD_BEST \
     --emb_init_std 1.0 \
-    # >> $LOG_FILE
+    --l2_reg_weight $L2_REG \
+    >> $LOG_FILE
 
 echo "[end at $(date)]" >> $LOG_FILE
+
+# [torch.nn.Linear(64, 1024), torch.nn.Tanh(), torch.nn.Linear(1024, 64)]
+# [torch.nn.Linear(64, 1024), torch.nn.Tanh(), torch.nn.Linear(1024, 1024), torch.nn.Tanh(), torch.nn.Linear(1024, 64)]
