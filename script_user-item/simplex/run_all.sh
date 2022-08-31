343e28ff26bfffe3e1c718100787fb446373e7fc
jianxun <v-xdeng@microsoft.com>
2022-08-31 09:16:13 +0000

    add hp search script for ultragcn and simplex

--- script_user-item/simplex/run_all.sh
+++ script_user-item/simplex/run_all.sh
@@ -2,15 +2,17 @@ DEVICE='cuda:2'
 
 DATASET='amazon-book-1.5m'
 
-reg=1e-4
+reg=1e-2
 num_neg=64
 neg_weight=64
 
 bash run_simplex.sh $DEVICE $DATASET $reg $num_neg $neg_weight
 
 
-# reg=1e-4
-# num_neg=64
-# neg_weight=16
+DATASET='taobao-1.6m'
 
-# bash run_simplex.sh $DEVICE $DATASET $reg $num_neg $neg_weight
+reg=1e-3
+num_neg=64
+neg_weight=64
+
+bash run_simplex.sh $DEVICE $DATASET $reg $num_neg $neg_weight
--- script_user-item/ultragcn/run_all.sh
+++ script_user-item/ultragcn/run_all.sh
@@ -8,7 +8,28 @@ lambda=0.8
 gamma=3.5
 reg=1e-5
 num_neg=256
-neg_weight=256
+neg_weight=64
+topk=8
+
+bash run_ultragcn.sh $DEVICE $DATASET $lambda $gamma $reg $num_neg $neg_weight $topk
+
+
+DATASET='taobao-1.6m'
+
+lambda=0.2
+gamma=1.5
+reg=1e-5
+num_neg=64
+neg_weight=64
+topk=8
+
+bash run_ultragcn.sh $DEVICE $DATASET $lambda $gamma $reg $num_neg $neg_weight $topk
+
+lambda=1.4
+gamma=1.5
+reg=1e-5
+num_neg=64
+neg_weight=64
 topk=8
 
 bash run_ultragcn.sh $DEVICE $DATASET $lambda $gamma $reg $num_neg $neg_weight $topk
