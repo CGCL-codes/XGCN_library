161979c2f25583101dad9659cf16866a48075729
jianxun <v-xdeng@microsoft.com>
2022-09-24 12:28:27 +0000

    adjust LFM data generation script

--- script_user-item/data/process_user-item_edge_list_dataset-LFM.sh
+++ script_user-item/data/process_user-item_edge_list_dataset-LFM.sh
@@ -1,5 +1,13 @@
+
 PROJECT_ROOT="/media/xreco/jianxun/xGCN"
-ALL_DATASETS_ROOT='/media/xreco/jianxun/xgcn_data/datasets'
+ALL_DATA_ROOT='/media/xreco/jianxun/xgcn_data'
+
+if [ $# -gt 1 ]
+  then 
+    PROJECT_ROOT=$1
+    ALL_DATA_ROOT=$2 
+fi
+
 
 DATASET='LFM'  ## 'taobao-1.6m' 'amazon-book-1.5m'
 
@@ -7,7 +15,7 @@ DATASET='LFM'  ## 'taobao-1.6m' 'amazon-book-1.5m'
 ######################################
 export PYTHONPATH=$PROJECT_ROOT
 
-DATA_ROOT=$ALL_DATASETS_ROOT'/instance_'$DATASET
+DATA_ROOT=$ALL_DATA_ROOT'/datasets/instance_'$DATASET
 # FILE_INPUT='/media/xreco/DEV/xiran/data/user_item_lightgcn/datasets/instance_taobao-1.6m/train_edges.txt'
 FILE_INPUT="$DATA_ROOT/train.tsv"
 FILE_OUTPUT="$DATA_ROOT/train.txt"
@@ -21,25 +29,25 @@ python $PROJECT_ROOT'/'data/handle_adj_graph_txt.py $PROJECT_ROOT \
     --file_input $FILE_OUTPUT \
 
 FILE_INPUT="$DATA_ROOT/valid.tsv"
-FILE_OUTPUT="$DATA_ROOT/val.txt"
-python $PROJECT_ROOT'/'data/convert_edgeList_2_adj.py $FILE_INPUT $FILE_OUTPUT 1 $'\t'
+if [[ -e $FILE_INPUT ]]; then   
+    FILE_OUTPUT="$DATA_ROOT/val.txt"
+    python $PROJECT_ROOT'/'data/convert_edgeList_2_adj.py $FILE_INPUT $FILE_OUTPUT 1 $'\t'
 
-FILE_INPUT=$DATA_ROOT'/val.txt'
-FILE_OUTPUT=$DATA_ROOT'/val.pkl'
+    FILE_INPUT=$DATA_ROOT'/val.txt'
+    FILE_OUTPUT=$DATA_ROOT'/val.pkl'
 
-python $PROJECT_ROOT'/'data/handle_adj_eval_set.py $PROJECT_ROOT \
-    --data_root $DATA_ROOT \
-    --dataset_type 'user-item' \
-    --dataset_name $DATASET \
-    --file_input $FILE_INPUT \
-    --file_output $FILE_OUTPUT \
+    python $PROJECT_ROOT'/'data/handle_adj_eval_set.py $PROJECT_ROOT \
+        --data_root $DATA_ROOT \
+        --dataset_type 'user-item' \
+        --dataset_name $DATASET \
+        --file_input $FILE_INPUT \
+        --file_output $FILE_OUTPUT \
 
-
-python $PROJECT_ROOT'/'data/sample_multi_pos_eval_set.py $PROJECT_ROOT \
-    --file_input $DATA_ROOT'/val.pkl' \
-    --file_output $DATA_ROOT'/val-1000.pkl' \
-    --num_sample 1000 \
-
+    python $PROJECT_ROOT'/'data/sample_multi_pos_eval_set.py $PROJECT_ROOT \
+        --file_input $DATA_ROOT'/val.pkl' \
+        --file_output $DATA_ROOT'/val-1000.pkl' \
+        --num_sample 1000 \
+fi
 #################################
 
 # FILE_INPUT="$DATA_ROOT/test_pos_edges.txt"
