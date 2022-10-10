ad3ca07c8e4045c2c3dca1812839ad57d6ce8414
jianxun <v-xdeng@microsoft.com>
2022-10-10 07:42:55 +0000

    small change in script

--- script/run_in_msra/abstract_run_xgcn.sh
+++ script/run_in_msra/abstract_run_xgcn.sh
@@ -1,12 +1,12 @@
-# source /opt/conda/bin/activate
-# conda env create --file=env/requirements.xgcn.yaml
-# conda activate xgcn 
+source /opt/conda/bin/activate
+conda env create --file=env/requirements.xgcn.yaml
+conda activate xgcn 
 
-# PROJECT_ROOT='./xGCN'
-# ALL_DATA_ROOT='/home/jialia/ds/social_and_user_item'
+PROJECT_ROOT='../..'
+ALL_DATA_ROOT='/home/jialia/ds/social_and_user_item'
 
-PROJECT_ROOT='/media/xreco/jianxun/xGCN'
-ALL_DATA_ROOT='/media/xreco/DEV/xiran/data/social_and_user_item'
+# PROJECT_ROOT='/media/xreco/jianxun/xGCN'
+# ALL_DATA_ROOT='/media/xreco/DEV/xiran/data/social_and_user_item'
 DEVICE='cuda'
 
 ########################################
