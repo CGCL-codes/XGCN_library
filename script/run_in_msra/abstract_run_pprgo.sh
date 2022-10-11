0dfefb0cacc90af7fb85615a7fca6916b3c60ce4
Jianxun-Lian <jianxun.lian@outlook.com>
2022-10-11 08:25:48 +0000

    uppdate script

--- script/run_in_msra/abstract_run_pprgo.sh
+++ script/run_in_msra/abstract_run_pprgo.sh
@@ -1,12 +1,10 @@
-# source /opt/conda/bin/activate
-# conda env create --file=env/requirements.xgcn.yaml
-# conda activate xgcn 
+source /opt/conda/bin/activate
+conda env create --file=env/requirements.xgcn.yaml
+conda activate xgcn 
 
-# PROJECT_ROOT='xGCN'
-# ALL_DATA_ROOT='/home/jialia/ds/social_and_user_item'
+PROJECT_ROOT='xGCN'
+ALL_DATA_ROOT='/home/jialia/ds/social_and_user_item'
 
-PROJECT_ROOT='/home/sxr/code/www23/xGCN'
-ALL_DATA_ROOT='/home/sxr/data/social_and_user_item'
 DEVICE='cuda'
 
 ########################################
@@ -14,5 +12,5 @@ DATASET=$1
 seed=$2
 use_ssnet=$3
 
-bash run_pprgo.sh $PROJECT_ROOT $ALL_DATA_ROOT $DEVICE $DATASET $seed $use_ssnet
+bash xGCN/script/run_in_msra/run_pprgo.sh $PROJECT_ROOT $ALL_DATA_ROOT $DEVICE $DATASET $seed $use_ssnet
 # xGCN/script/run_in_msra/
