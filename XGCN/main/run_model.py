25ef34249fceb72a48701e400e5eab313f1a5099
xiransong <xiransong@hust.edu.cn>
2023-03-01 11:37:41 +0800

    small changes

--- XGCN/main/run_model.py
+++ XGCN/main/run_model.py
@@ -1,10 +1,7 @@
+import XGCN
 from XGCN.utils.parse_arguments import parse_arguments
 from XGCN.utils.utils import print_dict, ensure_dir, set_random_seed
 from XGCN.utils import io
-from XGCN.model.build import build_Model
-from XGCN.dataloading.build import build_DataLoader
-from XGCN.evaluator.build import build_val_Evaluator, build_test_Evaluator
-from XGCN.train.Trainer import build_Trainer
 
 import os.path as osp
 
@@ -22,17 +19,16 @@ def main():
     set_random_seed(seed)
     
     data = {}  # containing some global data objects
-    data['data_root'] = config['data_root']
     
-    model = build_Model(config, data)
+    model = XGCN.build_Model(config, data)
     
-    train_dl = build_DataLoader(config, data)
+    train_dl = XGCN.build_DataLoader(config, data)
     
-    val_evaluator = build_val_Evaluator(config, data, model)
-    test_evaluator = build_test_Evaluator(config, data, model)
+    val_evaluator = XGCN.build_val_Evaluator(config, data, model)
+    test_evaluator = XGCN.build_test_Evaluator(config, data, model)
     
-    trainer = build_Trainer(config, data, model, train_dl,
-                            val_evaluator, test_evaluator)
+    trainer = XGCN.build_Trainer(config, data, model, train_dl,
+                                 val_evaluator, test_evaluator)
     
     trainer.train_and_test()
 
