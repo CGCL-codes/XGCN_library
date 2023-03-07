ed27252e3a6abd0e5e3da81354eda2bcd3ecc530
jianxun <v-xdeng@microsoft.com>
2023-03-07 07:33:38 +0000

    add PPRGo scripts

--- script/model/PPRGo/run_PPRGo-pokec.sh
+++ script/model/PPRGo/run_PPRGo-pokec.sh
@@ -1,37 +1,21 @@
-project_root="/media/xreco/DEV/xiran/code/XGCN"
-all_data_root="/media/xreco/DEV/xiran/data/XGCN"
+all_data_root=/media/xreco/DEV/xiran/code/XGCN_package_dev/XGCN_data
+config_file_root=/media/xreco/DEV/xiran/code/XGCN_package_dev/xGCN/config
 
-dataset="pokec"
-model="PPRGo"
+dataset=pokec
+model=PPRGo
+seed=0
 
-data_root=$all_data_root"/dataset/instance_"$dataset
+data_root=$all_data_root/dataset/instance_$dataset
+results_root=$all_data_root/model_output/$dataset/$model/[seed$seed]
 
-seed=1999
+file_pretrained_emb=$all_data_root/model_output/$dataset/Node2vec/[seed$seed]/out_emb_table.pt
 
-ppr_data_device="cuda:0"
-emb_table_device="cuda:0"
-forward_device="cuda:0"
-out_emb_table_device="cuda:0"
-
-results_root=$all_data_root"/model_output/"$dataset"/"$model"/debug"
-
-python $project_root/main/main.py $project_root \
-    --config_file $project_root"/model/"$model"/config.yaml" \
-    --seed $seed \
+python -m XGCN.main.run_model \
+    --config_file $config_file_root/$model-config.yaml \
     --data_root $data_root --results_root $results_root \
-    --ppr_data_root $all_data_root"/model_output/"$dataset"/PPR/undirected-top100" \
-    --val_evaluator "WholeGraph_OnePos_Evaluator" --val_batch_size 256 \
-    --file_val_set $data_root"/val_edges-1000.pkl" \
-    --test_evaluator "WholeGraph_MultiPos_Evaluator" --test_batch_size 256 \
-    --file_test_set $data_root"/test.pkl" \
-    --epochs 200 --val_freq 1 \
-    --ppr_data_device $ppr_data_device \
-    --emb_table_device $emb_table_device \
-    --forward_device $forward_device \
-    --out_emb_table_device $out_emb_table_device \
-    --from_pretrained 0 --file_pretrained_emb "" \
-    --freeze_emb 0 --use_sparse 0 \
-    --emb_lr 0.01 \
-    --topk 32 --use_uniform_weight 1 \
-    --loss_type "bpr" \
-    --L2_reg_weight 0.0 \
+    --val_evaluator WholeGraph_OnePos_Evaluator --val_batch_size 256 \
+    --file_val_set $data_root/val_edges.pkl \
+    --test_evaluator WholeGraph_MultiPos_Evaluator --test_batch_size 256 \
+    --file_test_set $data_root/test_set.pkl \
+    --from_pretrained 0 --file_pretrained_emb $file_pretrained_emb \
+    --ppr_data_root $all_data_root/model_output/$dataset/PPR/[seed0] \
