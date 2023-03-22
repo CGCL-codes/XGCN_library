e6a45ab9a92c9ac04f1cf048ba9dd3a95cd118a3
luozhhh <luozhhh@hust.edu.cn>
2023-03-22 09:26:16 +0800

    add data.reindex

---
+++ XGCN/data/reindex/__init__.py
@@ -0,0 +1,2 @@
+from .reindex import reindex_homogeneous_graph, reindex_user_item_graph
+from .ReIndexDict import ReIndexDict
---
+++ XGCN/data/reindex/reindex.py
@@ -0,0 +1,39 @@
+from .ReIndexDict import ReIndexDict
+from XGCN.utils import io
+from XGCN.utils.utils import wc_count
+
+import numpy as np
+from tqdm import tqdm
+
+
+def reindex_homogeneous_graph(filename, skiprows=0, delimiter=None):
+    id_mapping = ReIndexDict()
+    num_edges = wc_count(filename) - skiprows
+    E_src = np.empty(num_edges, dtype=np.int32)
+    E_dst = np.empty(num_edges, dtype=np.int32)    
+    with open(filename, 'r') as f:
+        for _ in range(skiprows):
+            f.readline()
+        for i in tqdm(range(num_edges)):
+            s = f.readline().split(delimiter)
+            raw_src, raw_dst = int(s[0]), int(s[1])
+            E_src[i] = id_mapping[raw_src]
+            E_dst[i] = id_mapping[raw_dst]
+    return E_src, E_dst, id_mapping
+
+
+def reindex_user_item_graph(filename, skiprows=0, delimiter=None):
+    user_id_mapping = ReIndexDict()
+    item_id_mapping = ReIndexDict()
+    num_edges = wc_count(filename) - skiprows
+    E_src = np.empty(num_edges, dtype=np.int32)
+    E_dst = np.empty(num_edges, dtype=np.int32)
+    with open(filename, 'r') as f:
+        for _ in range(skiprows):
+            f.readline()
+        for i in tqdm(range(num_edges)):
+            s = f.readline().split(delimiter)
+            raw_src, raw_dst = int(s[0]), int(s[1])
+            E_src[i] = user_id_mapping[raw_src]
+            E_dst[i] = item_id_mapping[raw_dst]
+    return E_src, E_dst, user_id_mapping, item_id_mapping
