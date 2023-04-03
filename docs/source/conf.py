9d51129b1b9ed8b012b2f64ef9ba8c29b25e5c38
xiransong <xiransong@hust.edu.cn>
2023-04-03 06:52:05 +0000

    adding docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../XGCN'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 
 # -- Project information -----------------------------------------------------
